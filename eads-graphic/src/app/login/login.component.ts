58f73ac4f136331d8ede67b7932e41fc6f0c5941
Tyler Eads <tyler.eads9@gmail.com>
2018-06-10 09:21:34 -0400

    Login and Logout Implementation

--- eads-graphic/src/app/login/login.component.ts
+++ eads-graphic/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'eg-login',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // Login Component Properties
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+
+        this.flashMessage.show('You are now loged in', {
+          cssClass: 'alert-success',
+          timeout: 3000
+        });
+
+        // Redirects User To Login Screen
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.flashMessage.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        });
+
+        // Redirects User To Login Screen
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
 }
