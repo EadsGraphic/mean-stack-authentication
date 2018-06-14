b82341044d9397cd037f30edef856b6fd41a383b
Tyler Eads <tyler.eads9@gmail.com>
2018-06-13 21:25:27 -0400

    Prod Config & CDN Imports

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
+// Heroku Config Variables
+let MLAB_ID = process.env.MLAB_ID;
+let MLAB_PSWD = process.env.MLAB_PSWD;
+
+
 module.exports = {
-    database: "mongodb://admin:adminPassword#1@ds153752.mlab.com:53752/eadsgraphic-auth-app",
+    database: "mongodb://" + MLAB_ID + ":" + MLAB_PSWD + "@ds153752.mlab.com:53752/eadsgraphic-auth-app",
     secret: "mysecretkey"
 };
 // database: 'mongodb://localhost:27017/meanauth', //Dev
