1cbc8471e375e99086eb08377e29c105fa3ab57e
Tyler Eads <tyler.eads9@gmail.com>
2018-06-15 12:12:35 -0400

    Component Content Creation

--- eads-graphic/src/app/app.module.ts
+++ eads-graphic/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AuthGuard } from './guards/auth.guard';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { AuthGuard } from './guards/auth.guard';
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
