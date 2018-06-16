5ce5e9b7e8ef2656c93d9c78874445334d8c2d86
Tyler Eads <tyler.eads9@gmail.com>
2018-06-16 09:27:56 -0400

    Implemented basic landing page

--- eads-graphic/src/app/home/home.component.ts
+++ eads-graphic/src/app/home/home.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'eg-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', './tabler-io.css']
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
