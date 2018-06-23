8872e1bc9cc642643066af00cd43589ba675e113
Tyler Eads <tyler.eads9@gmail.com>
2018-06-23 09:25:18 -0400

    database.js variable updates

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
-// Heroku Config Variables
-let MLAB_ID = process.env.MLAB_ID;
-let MLAB_PSWD = process.env.MLAB_PSWD;
+// Production Config Variables
+const MLAB_ID = process.env.MLAB_ID;
+const MLAB_PSWD = process.env.MLAB_PSWD;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-
+// Production
 module.exports = {
     database: "mongodb://" + MLAB_ID + ":" + MLAB_PSWD + "@ds153752.mlab.com:53752/eadsgraphic-auth-app",
-    secret: "mysecretkey"
+    secret: JWT_SECRET
 };
-// database: 'mongodb://localhost:27017/meanauth', //Dev
+
+// Development
+// module.exports = {
+//     database: 'mongodb://localhost:27017/meanauth',
+//     secret: "93F751EE67BF682261FC28E26817F"
+// }
